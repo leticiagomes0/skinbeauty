942878597f7473516ae75eae3a6f696b7e41f06d
leticiagomes0 <ferreiragomesleticia89@gmail,com>
2022-11-22 21:44:13 -0300

    projeto skinbeauty rev.5

---
+++ js/map.js
@@ -0,0 +1,17 @@
+jQuery(function ($) {
+    function init_map1() {
+        var myLocation = new google.maps.LatLng(-23.4865071,-47.4510347,17);
+        var mapOption = {
+            center: myLocation,
+            zoom: 16
+        };
+        var marker = new google.maps.Marker({
+            position: myLocation,
+            title: "localização"
+        });
+        var map = new google.maps.Map(document.getElementById("map1"),
+        mapOption);
+        marker.setMap(map);
+    }
+    init_map1();
+});
